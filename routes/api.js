1e380799b321299ce3ab4f05e5602566f4612831
Ramilbey <ramilbey505711503@gmail.com>
2025-05-03 16:24:04 +0800

    create rotes folder & api.js for runnning

---
+++ routes/api.js
@@ -0,0 +1,9 @@
+// routes/api.js
+const express = require('express');
+const router = express.Router();
+
+router.get('/hello', (req, res) => {
+  res.json({ message: 'Hello from the API!' });
+});
+
+module.exports = router;
